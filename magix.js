5c3d230540663ecf9495c73c447937bfafb97111
Leo Z <72562754+plasma4@users.noreply.github.com>
2024-06-15 21:53:00 +0800

    Fixed a silly issue

--- magix.js
+++ magix.js
@@ -26,7 +26,7 @@ meta.name = "viewport";
 meta.content = "width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no";
 document.getElementsByTagName('head')[0].appendChild(meta);
 
-const isChangedMagix = true
+var isChangedMagix = true
 var clickModePolicy = 0
 var clickModeUnit = 0
 // Allow touchscreen or mobile users to change policies
