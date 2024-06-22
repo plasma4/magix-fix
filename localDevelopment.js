b0b72aa675a45eb75151f8f4e7ff710af3686bd2
Leo Z <72562754+plasma4@users.noreply.github.com>
2024-06-22 11:42:15 +0800

    Fixed the contents inside

--- localDevelopment.js
+++ localDevelopment.js
@@ -1,4 +1,4 @@
 // THIS IS FOR LOCALLY MODIFYING MAGIX AND SHOULD NOT BE CHANGED WHEN PLAYING NORMALLY.
 // If you wish to force Magix to use your local files (magix.js and magixUtils.js), set the value below to true. This is not advised because the game will automatically use the newest version when possible and use localStorage to store your mod files. You should only use this if you are trying to modify Magix.
 
-var offlineMode = true
+var offlineMode = false
