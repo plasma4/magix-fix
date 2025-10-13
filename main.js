2332d310b991a34ea60c492691a0e9f25f6aeec8
painekillers <blablablapandas@gmail.com>
2025-10-13 14:15:49 -0700

    Update main.js

--- main.js
+++ main.js
@@ -1435,7 +1435,7 @@ G.Launch=function()
 	}
 	G.SelectMagix=function()
 	{
-		G.NewGame(false,['https://raw.githubusercontent.com/plasma4/magix-fix/master/magixUtils.js', 'https://raw.githubusercontent.com/plasma4/magix-fix/master/magix.js']);
+		G.NewGame(false,['https://raw.githubusercontent.com/painekillers/magix-fix/master/magixUtils.js', 'https://raw.githubusercontent.com/painekillers/magix-fix/master/magix.js']);
 	}
 	G.SelectMods=function()
 	{
@@ -8360,4 +8360,5 @@ window.onload=function()
 			G.LoadResources();
 		}
 	}
+
 };
